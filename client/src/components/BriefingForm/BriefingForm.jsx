bdaf28ba1cc3dc3b7ac1e6c5019ea03d35567875
kassioL2L <kassio_leite2@hotmail.com>
2024-03-17 19:26:34 -0300

    ajeitando erro cors

--- client/src/components/BriefingForm/BriefingForm.jsx
+++ client/src/components/BriefingForm/BriefingForm.jsx
@@ -1,25 +1,32 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import { updateBriefingById, createBriefing } from '../../services/api';
 import './BriefingForm.css';
 
 /* eslint-disable react/prop-types */
-const BriefingForm = ({ id }) => { 
-    const [clientName, setclientName] = useState('');
-    const [description, setdescription] = useState('');
-    const [dateTime] = useState(new Date().toISOString()); 
+const BriefingForm = ({ id }) => {
+    const [clientName, setClientName] = useState('');
+    const [description, setDescription] = useState('');
+    const [dateTime] = useState(new Date().toISOString());
+
+    const generateId = () => {
+        return Math.floor(Math.random() * 1000000); 
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
+        // Gere um novo ID se o ID não estiver definido
+        const newId = id || generateId();
+
         const briefingData = {
-            id, 
+            id: newId,
             clientName,
             description,
             dateTime,
         };
 
         try {
-            // Check if briefingid exists, if so, update the briefing
+            // Check if briefing id exists, if so, update the briefing
             if (id) {
                 await updateBriefingById(id, briefingData);
             } else {
@@ -31,17 +38,17 @@ const BriefingForm = ({ id }) => {
     };
 
     return (
-        <div  className="container">
+        <div className="container">
             <form onSubmit={handleSubmit}>
                 <input
                     type="text"
                     value={clientName}
-                    onChange={(e) => setclientName(e.target.value)}
+                    onChange={(e) => setClientName(e.target.value)}
                     placeholder="Nome do Cliente"
                 />
                 <textarea
                     value={description}
-                    onChange={(e) => setdescription(e.target.value)}
+                    onChange={(e) => setDescription(e.target.value)}
                     placeholder="Descrição"
                 />
                 <p>Data/Hora: {new Date(dateTime).toLocaleString()}</p>
@@ -50,7 +57,5 @@ const BriefingForm = ({ id }) => {
         </div>
     );
 };
-
-
 
 export default BriefingForm;
