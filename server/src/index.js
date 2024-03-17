67322d9b649d74bd7b751773d9beb666f9dd561b
kassioL2L <kassio_leite2@hotmail.com>
2024-03-17 19:31:09 -0300

    ajeitando erro cors

--- server/src/index.js
+++ server/src/index.js
@@ -2,24 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const briefingRoutes = require('./routes/briefingRoutes');
 const mongoose = require('mongoose');
+const cors = require('cors'); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('Bem-vindo ao servidor de gestão de briefings!');
-  });  
+});
 
 // Middleware para análise de corpos de solicitação
 app.use(bodyParser.json());
+
+// Middleware CORS para permitir solicitações somente da origem 5173
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 
 // Rotas da API de briefings
 app.use('/briefings', briefingRoutes);
 
 // Configurações do MongoDB
 mongoose.connect('mongodb+srv://kassiolimaprofissional:nLSL3xOD5WUBsXOm@briefing.6cmdehh.mongodb.net/')
-
-
+  .then(() => console.log('MongoDB conectado'))
+  .catch(err => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
